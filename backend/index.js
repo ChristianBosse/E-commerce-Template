f89f562dbef0e375a4acaaaa47ad7d688c818d2c
Christian Bosse <christianbosse1998@gmail.com>
2023-10-13 01:35:48 -0300

    Product API Implementation

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const products = require("./data/products");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
