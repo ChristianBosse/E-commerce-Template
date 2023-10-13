097eca3f54c9a43a6f03d2c2d1e81e5b7952c2bf
Christian Bosse <christianbosse1998@gmail.com>
2023-10-13 15:00:26 -0300

    Data model implemented

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchmea = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchmea);
+
+module.exports = User;
