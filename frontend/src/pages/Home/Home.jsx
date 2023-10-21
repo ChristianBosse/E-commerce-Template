55495a79b6fb65963e12fb82288f55858127d79c
Christian Bosse <christianbosse1998@gmail.com>
2023-10-21 12:15:40 -0300

    Redux API Slice

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,33 +1,28 @@
-import React, { useEffect, useState } from "react";
+import { useGetProductsQuery } from "../../slices/productsApiSlice";
 import { Row, Col } from "react-bootstrap";
 import Products from "../../components/Products";
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        const fetchProducts = async () => {
-            fetch("http://localhost:5000/api/products")
-                .then((res) => res.json())
-                .then((data) => {
-                    console.log(data);
-                    setProducts(data);
-                });
-        };
-
-        fetchProducts();
-    }, []);
+    const { data: products, isLoading, error } = useGetProductsQuery();
 
     return (
         <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Products product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <div>{error?.data?.message || error.error}</div>
+            ) : (
+                <>
+                    <h1>Latest Products</h1>
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Products product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                </>
+            )}
         </>
     );
 };
