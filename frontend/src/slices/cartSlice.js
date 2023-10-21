3ee011de613304fa8960aaf6003186fcd4ff84f5
Christian Bosse <christianbosse1998@gmail.com>
2023-10-21 13:48:05 -0300

    Fixed Redux bug

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
     ? JSON.parse(localStorage.getItem("cart"))
     : { cartItems: [] };
-
-const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
     name: "cart",
@@ -18,38 +15,14 @@ const cartSlice = createSlice({
             const existItem = state.cartItems.find((x) => x._id === item._id);
 
             if (existItem) {
-                state.carItems = state.cartItems.map((x) =>
+                state.cartItems = state.cartItems.map((x) =>
                     x._id === existItem._id ? item : x
                 );
             } else {
-                state.cartItems = [...state.carItems, item];
+                state.cartItems = [...state.cartItems, item];
             }
 
-            //Calculate items price
-            state.itemsPrice = addDecimals(
-                state.cartItems.reduce((acc, item) => {
-                    acc + item.price * item.qty;
-                })
-            );
-
-            //Calculate shipping price (Over $99 is free shipping)
-            state.shippingPrice = addDecimals(state.itemsPrice > 99 ? 0 : 8);
-
-            //Calculate tax price (15% tax)
-            state.taxPrice = addDecimals(
-                Number((0.15 * state.itemsPrice).toFixed(2))
-            );
-
-            //Calculate total price
-            state
-                .totalPrice(
-                    Number(state.itemsPrice) +
-                        Number(state.shippingPrice) +
-                        Number(state.taxPrice)
-                )
-                .toFixed(2);
-
-            localStorage.setItem("cart", JSON.stringify(state));
+            return updateCart(state);
         },
     },
 });
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,10 @@ import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
 
 const store = configureStore({
-    reducer: { [apiSlice.reducerPath]: apiSlice.reducer },
-    cart: cartSliceReducer,
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSliceReducer,
+    },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
