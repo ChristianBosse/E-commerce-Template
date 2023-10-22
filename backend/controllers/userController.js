156680b6148b19905759744197a6dd08e3b0835c
Christian Bosse <christianbosse1998@gmail.com>
2023-10-22 02:23:22 -0300

    Added Logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,12 @@ const registerUser = asyncHandler(async (req, res) => {
 //Post /api/users/logout
 //Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send("Logout User");
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: "Logged out successfully" });
 });
 
 //Get user profile
