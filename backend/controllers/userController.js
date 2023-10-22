e8698ee09f14346b659889f0472b8e5b5b4aee3a
Christian Bosse <christianbosse1998@gmail.com>
2023-10-22 01:11:17 -0300

    Added auth routes logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,21 @@ const User = require("../models/userModel");
 //Post /api/users/login
 //Public
 const authUser = asyncHandler(async (req, res) => {
-    res.send("Auth User");
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 //Register a new user
--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 connectDB(); // Connect to MongoDB
 
 app.get("/", (req, res) => {
