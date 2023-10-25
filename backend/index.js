e2978ebd5763269785e5b950d0d61c963d65a568
Christian Bosse <christianbosse1998@gmail.com>
2023-10-25 08:27:28 -0300

    Added logout logic + fixed proxy

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 //body parser
 app.use(express.json());
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/vite.svg";
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
     const { userInfo } = useSelector((state) => state.auth);
 
-    const logoutHandler = () => {
-        console.log("logout");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate("/login");
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
