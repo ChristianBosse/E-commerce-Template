3b2f7c844bd1c69ba9603a50e8cee458b6b54962
Christian Bosse <christianbosse1998@gmail.com>
2023-10-25 07:44:46 -0300

    added Login method

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -5,7 +5,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: USERS_URL / login,
+                url: `${USERS_URL}/login`,
                 method: "POST",
                 body: data,
             }),
